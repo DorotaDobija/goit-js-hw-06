3d9228ce7250faaf0c1af4211c5b4c0cd5bfa09b
DorotaDobija <dorota.dobija93@gmail.com>
2023-03-24 22:14:51 +0100

    task 09 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,15 @@
+const btnEl = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
+const bodyEl = document.querySelector("body");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+btnEl.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  spanEl.textContent = randomColor;
+  bodyEl.style.backgroundColor = randomColor;
+});
