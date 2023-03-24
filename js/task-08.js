1c3bf4e33c18f96c3f08a582f3cfd5fdf1595d27
DorotaDobija <dorota.dobija93@gmail.com>
2023-03-24 21:52:27 +0100

    task 08 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,14 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const {
+    elements: { email, password },
+  } = e.currentTarget;
+  if (email.value === "" || password.value === "") {
+    alert("Please complete all fields!");
+  }
+  console.log(`{email: ${email.value},
+password: ${password.value}}`);
+  e.currentTarget.reset();
+});
